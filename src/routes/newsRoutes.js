49e1811c91313e90c666130355baf176b3fcdc24
ERRAOUCHA <yassineerraoucha5@gmail.com>
2025-01-06 08:14:55 +0100

    Compléter les routes  existe et ajouter des autres pour la modification et la suppression

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const newsController = require('../controllers/newsController');
 
 // TODO: Question 4 - Compléter les routes
+ 
+// Route pour récupérer tous les articles
 router.get('/', newsController.getAllNews);
+
+// Route pour récupérer un article par son ID
 router.get('/:id', newsController.getNewsById);
+
+// Route pour créer un nouvel article
 router.post('/', newsController.createNews);
+
+// Route pour mettre à jour un article existant
+router.put('/:id', newsController.updateNews);
+
+// Route pour supprimer un article
+router.delete('/:id', newsController.deleteNews);
+
 
 module.exports = router;
